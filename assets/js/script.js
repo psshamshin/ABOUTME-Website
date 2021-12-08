ef0a19a349076a9fc8f3c8b3e36091eb4a5bbbb8
psshamshin <57655945+psshamshin@users.noreply.github.com>
2021-12-08 10:03:01 +0300

    v 1.4
    draggable and editable cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//========JS========
 var cart = {
 		'good': []
 	};
@@ -48,12 +49,15 @@ function check(a) {
 		}
 	}
 	if (a <= 2){ a += 1; check(); }
-	else{ print(); }
+	else{
+		print();
+	}
 }
 
 function clear_storage(){
 	sessionStorage.clear();
-	document.getElementById('output_1').value = '';
+	//document.getElementById('output_1').value = '';
+	//cart_render();
 }
 
 function print() {
@@ -65,5 +69,254 @@ function print() {
 		text += curr.name+' '+resolution+' $'+curr.cost+' x'+curr.num+' '+'\n';
 		sum += cart.good[i].cost * cart.good[i].num;
 	}
-	document.getElementById('output_1').value = text + '\n' + 'sum: $' + sum;
+	//document.getElementById('output_1').value = text + '\n' + 'sum: $' + sum;
 }
+
+function del(name){
+	//alert(name);
+	cart = JSON.parse(sessionStorage.getItem('cart'));
+	for (let i=0; i < cart.good.length; i++){
+		if (cart.good[i].name == name){
+			cart.good.splice(i,1);
+		}
+	}
+	sessionStorage.setItem('cart', JSON.stringify(cart));
+	my_func();
+}
+
+function p(name) {
+	//alert(name);
+	cart = JSON.parse(sessionStorage.getItem('cart'));
+	for (let i=0; i < cart.good.length; i++){
+		if (cart.good[i].name == name){
+			cart.good[i].num += 1;
+			//alert('fff');
+			break;
+		}
+	}
+	sessionStorage.setItem('cart', JSON.stringify(cart));
+	//cart_render();
+	my_func();
+}
+
+function m(name) {
+	//alert(name);
+	cart = JSON.parse(sessionStorage.getItem('cart'));
+	for (let i=0; i < cart.good.length; i++){
+		if (cart.good[i].name == name){
+			if (cart.good[i].num == 1){
+				del(name);
+			}
+			else{
+				cart.good[i].num -= 1;
+			}
+			//alert('fff');
+			break;
+		}
+	}
+	sessionStorage.setItem('cart', JSON.stringify(cart));
+	//cart_render();
+	my_func();
+}
+
+my_func = null;
+
+
+function writeFile(name, value) {
+	//alert('e');
+	var val = value;
+	if (value === undefined) {
+		val = "";
+	}
+	var download = document.createElement("a");
+	download.href = "data:text/plain;content-disposition=attachment;filename=file," + val;
+	download.download = name;
+	download.style.display = "none";
+	download.id = "download"; document.body.appendChild(download);
+	document.getElementById("download").click();
+	document.body.removeChild(download);
+}
+
+
+
+
+//========JQuery========
+//$('#left_but').on('click', alert('gg'));
+$(document).ready(function(){
+	$(".shop_item").draggable(
+		{
+			cursor: "move",
+			helper: "clone",
+			scroll: true,
+			containment: "body",
+		}
+	);
+
+	$(".cart_graggable").droppable(
+		{
+			accept: ".shop_item",
+			classes: {
+				"ui-droppable-active": "ui-state-active",
+				"ui-droppable-hover": "ui-state-hover"
+			},
+
+			drop: function (event, ui) {
+				var str = ui.draggable.attr("id").slice(0, -2);
+				add_to_cart(str, 10);
+				//alert(ui.draggable.attr("id"));
+				//alert(str);
+			}
+		}	
+	);
+
+
+	// ====== ГАЛЕРЕЯ ====== 
+	var i = 0;
+	var ar_gallery = [
+		'url(assets/img/macintoch.png)',
+		'url(assets/img/cup.png)',
+		'url(assets/img/xmas.png)'
+	];
+	function left(){
+		alert('aa');
+		$('.section--gallery').css('background-image', ar_gallery[i]);
+		(i==ar_gallery.length-1) ? i=0 : i++;
+	}
+	function right() {
+		$('.section--gallery').css('background-image', ar_gallery[i]);
+		(i==0) ? i=ar_gallery.length-1 : i--;
+	}
+	/*
+	function search(text){
+		alert('bg');
+		//my_func();
+	}
+	*/
+	$("#but_search").click(function () {
+		var goods = [
+			"red_i",
+			"white_i",
+			"violet_i",
+			"green_i",
+			"blue_i",
+			"pink_i"
+		];
+		var flag = false;
+		var search_item = $("#inp_text").val();
+		//alert(search_item);
+		//alert(goods[0]);
+		for (var i=0; i < goods.length; i++){
+			if ((search_item + "_i") == goods[i]){
+				flag = true;
+				//alert('g');
+				break;
+			}
+		}
+		if (flag){
+			//alert('f');
+			for(var i = 0; i < goods.length; i++){
+				if ((search_item + "_i") != goods[i]){
+					//alert(goods[i]);
+					$("#" + goods[i]).css('opacity', '0.2');
+					$("#" + goods[i] + "s").css('opacity', '0.2');
+				}
+			}
+		}
+		if (search_item == ""){
+			for(var i=0; i < goods.length; i++){
+				$("#" + goods[i]).css('opacity', '1');
+				$("#" + goods[i] + "s").css('opacity', '1');
+			}
+		}
+		
+	})
+
+	// ====== КОРЗИНА ====== 
+	
+	function cart_render(){
+		bill = '';
+		sum = 0;
+		$('#inner_dark_plate').empty();
+		cart = JSON.parse(sessionStorage.getItem('cart'));
+		//alert(cart.good[0]);
+		if (cart.good[0] == null){
+			$('#inner_dark_plate').append('<h1 class="text_good">Empty</h1>');
+		}
+		//alert(cart.good[0].name);
+		var images = [
+		'<img src="assets/img/city_caramel.jpg" class="img_cart">',
+		'<img src="assets/img/city_pink.png" class="img_cart">',
+		'<img src="assets/img/city_violet.jpg" class="img_cart">',
+		'<img src="assets/img/city_dark_red.jpg" class="img_cart">',
+		'<img src="assets/img/city_green.png" class="img_cart">',
+		'<img src="assets/img/city_blue.png" class="img_cart">'
+		];
+
+		var text_start = '<h1 class="text_good">';
+		var text_end = '</h1>';
+		
+		for (let i=0; i < cart.good.length; i++){
+			parent_st = '<div class="cart_item" id="cart_item';
+			parent_end = '"></div>';
+			$('#inner_dark_plate').append(parent_st+i+parent_end);
+			let num_of_img = 0;
+			
+			switch (cart.good[i].name){
+				case 'white': num_of_img=0; break;
+				case 'pink': num_of_img=1; break;
+				case "purple": num_of_img=2; break;
+				case 'red': num_of_img=3; break;
+				case 'green': num_of_img=4; break;
+				case 'blue': num_of_img=5; break;
+			}
+			
+			$('#cart_item'+i).append(images[num_of_img]);
+			$('#cart_item'+i).append(text_start+cart.good[i].name+text_end);
+			bill += cart.good[i].name + ' ';
+			var resolution = (cart.good[i].res==1) ? '(1920x1920)':'(1080x1080)';
+
+			$('#cart_item'+i).append(text_start+resolution+text_end);
+			bill += resolution + ' ';
+
+			if (cart.good[i].res==1){
+				var cost = cart.good[i].cost*2*cart.good[i].num;
+			}
+			else{
+				var cost = cart.good[i].cost*cart.good[i].num;
+			}
+			$('#cart_item'+i).append(text_start+'$'+cost+text_end);
+			bill += '$' + cost + ' ';
+			sum += cost;
+
+			var count1_start = '<h1 class="text_good" onclick="m(';
+
+			$('#cart_item'+i).append(count1_start+"'"+cart.good[i].name+"'"+')">-</h1>');
+
+
+			
+			$('#cart_item'+i).append(text_start+cart.good[i].num+text_end);
+			bill += 'num:' + ' ' + cart.good[i].num + '\n';
+			//alert(bill);
+
+
+
+			var count_start = '<h1 class="text_good" onclick="p(';
+
+			$('#cart_item'+i).append(count_start+"'"+cart.good[i].name+"'"+')">+</h1>');
+
+			var can_but = '<img src="assets/img/cancel.png"height="50%"class="cancel_button" onclick = "del(';
+
+			//alert('ff'+cart.good[i].name);
+			$('#cart_item'+i).append(can_but+"'"+cart.good[i].name+"'"+')">');
+		}
+		bill += 'sum: $' + sum;
+		my_func = cart_render;
+	}
+	//$("#cart_but").on('click', cart_render);
+	check(2);
+	cart_render();
+
+	$('#left_but').on('click', left);
+	$('#right_but').on('click', right);
+	
+});
